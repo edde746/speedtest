b3fbfba7ead0beca61257e2be3c66f0446243931
edde746 <86283021+edde746@users.noreply.github.com>
2024-01-19 01:56:44 +0100

    stat cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Download, Timer, Upload } from "@steeze-ui/lucide-icons";
+  import Stat from "./Stat.svelte";
+</script>
+
+<div class="p-4 grid grid-cols-6 gap-4">
+  <Stat title="Ping" value="13 ms" icon={Timer} trend={-5.3} />
+  <Stat title="Download" value="502.2 mbps" icon={Download} trend={3.2} />
+  <Stat title="Upload" value="47.1 mbps" icon={Upload} trend={7.5} />
+</div>
---
+++ src/routes/Stat.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import {
+    TrendingDown,
+    TrendingUp,
+    MoveRight,
+    type IconSource,
+  } from "@steeze-ui/lucide-icons";
+  import { Icon } from "@steeze-ui/svelte-icon";
+
+  const { title, value, icon, trend } = $props<{
+    title: string;
+    value: string;
+    icon: IconSource;
+    trend: number;
+  }>();
+
+  type Trend = "up" | "neutral" | "down";
+  const getTrend = (trend: number) => {
+    if (trend > 5) return { class: "text-success", icon: TrendingUp };
+    if (trend < -5) return { class: "text-error", icon: TrendingDown };
+    return { class: "text-primary", icon: MoveRight };
+  };
+
+  const trendStyle = $derived(getTrend(trend));
+</script>
+
+<div
+  class="rounded-box bg-base-200 p-4 col-span-2 flex justify-between items-center"
+>
+  <div>
+    <span class="opacity-80">{title}</span>
+    <h3 class="text-xl">{value}</h3>
+    <div class="flex items-center gap-2 {trendStyle.class}">
+      <Icon src={trendStyle.icon} class="w-5 h-5" />
+      <span class="text-xs">{trend}%</span>
+    </div>
+  </div>
+  <Icon src={icon} class="{trendStyle.class} w-10 h-10" />
+</div>
